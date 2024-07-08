0495a385bab25c867d9dda3ade85bb540c26446b
MAC <andresbuendia560@gmail.com>
2024-07-08 17:49:42 -0500

    Configure deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/portafolio/",
+    base: "/portafolio.github.io/",
     plugins: [react()],
     build: {
         rollupOptions: {
