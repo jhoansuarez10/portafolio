95eec93c05384732ee7dc2226af80dda72a8dd07
MAC <andresbuendia560@gmail.com>
2024-07-08 12:13:09 -0500

    firts commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,179 @@
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import ReactDOMServer from "react-dom/server";
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+import ContactEmailTemplate from "./ContactEmailTemplate";
+
+const Contact = () => {
+    const formRef = useRef();
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const [loading, setLoading] = useState(false);
+    const [emailSent, setEmailSent] = useState(false);
+    const [errors, setErrors] = useState({
+        name: false,
+        email: false,
+        message: false,
+    });
+
+    const emailContact = ReactDOMServer.renderToString(<ContactEmailTemplate message={form.message} />);
+    const emailConfig = {
+        subject: `Thank You for Contacting me, ${form.name.split(" ")[0]} 🚀`,
+        from: "Andrés Buendia",
+        receiverEmail1: "andresbuendia561@gmail.com",
+    };
+
+    const handleChange = (e) => {
+        const { target } = e;
+        const { name, value } = target;
+
+        setForm({
+            ...form,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let hasErrors = false;
+        const newErrors = {
+            name: form.name === "",
+            email: form.email === "",
+            message: form.message === "",
+        };
+
+        if (newErrors.name || newErrors.email || newErrors.message) {
+            hasErrors = true;
+        }
+
+        setErrors(newErrors);
+
+        if (hasErrors) {
+            return;
+        }
+
+        setLoading(true);
+
+        const data = {
+            name: form.name,
+            email: form.email,
+            companyId: "Gabriel",
+            office: "Portfolio",
+            emailConfig: emailConfig,
+            htmlContactTemplate: emailContact,
+        };
+
+        fetch("https://videface-server-cars-mqx4ohbz2q-ue.a.run.app/send-contact-email", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then(
+                () => {
+                    setLoading(false);
+                    setEmailSent(true);
+
+                    setForm({
+                        name: "",
+                        email: "",
+                        message: "",
+                    });
+                },
+                (error) => {
+                    setLoading(false);
+                    console.error(error);
+
+                    alert(
+                        "Ahh, something went wrong. Kindly notify me that the email service is not working so I can fix it ASAP!"
+                    );
+                }
+            );
+    };
+
+    return (
+        <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+            <motion.div
+                variants={slideIn("left", "tween", 0.2, 1)}
+                className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+            >
+                <p className={styles.sectionSubText}>Get in touch</p>
+                <h3 className={styles.sectionHeadText}>Contact me.</h3>
+
+                <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
+                    <label className="flex flex-col">
+                        <span className="text-white font-medium mb-4">Your Name</span>
+                        <input
+                            type="text"
+                            name="name"
+                            value={form.name}
+                            onChange={handleChange}
+                            placeholder="Nice to meet you! 🤝"
+                            className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium ${
+                                errors.name ? "border-red-500" : ""
+                            }`}
+                        />
+                        {errors.name && <span className="text-red-500">Please, write your name here.</span>}
+                    </label>
+                    <label className="flex flex-col">
+                        <span className="text-white font-medium mb-4">Your email</span>
+                        <input
+                            type="email"
+                            name="email"
+                            value={form.email}
+                            onChange={handleChange}
+                            placeholder="Won't spam you! 🙌"
+                            className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium ${
+                                errors.email ? "border-red-500" : ""
+                            }`}
+                        />
+                        {errors.email && <span className="text-red-500">Don't forget to write your email.</span>}
+                    </label>
+                    <label className="flex flex-col">
+                        <span className="text-white font-medium mb-4">Your Message</span>
+                        <textarea
+                            rows={7}
+                            name="message"
+                            value={form.message}
+                            onChange={handleChange}
+                            placeholder="What you want to say?"
+                            className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium ${
+                                errors.message ? "border-red-500" : ""
+                            }`}
+                        />
+                        {errors.message && (
+                            <span className="text-red-500">The message can't be empty. Please, don't be shy!</span>
+                        )}
+                    </label>
+
+                    <button
+                        type="submit"
+                        className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+                        disabled={emailSent}
+                    >
+                        {loading ? "Sending..." : emailSent ? "Email sent successfully 🚀" : "Send"}
+                    </button>
+                </form>
+            </motion.div>
+
+            <motion.div
+                variants={slideIn("right", "tween", 0.2, 1)}
+                className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+            >
+                <EarthCanvas />
+            </motion.div>
+        </div>
+    );
+};
+
+export default SectionWrapper(Contact, "contact");
---
+++ src/components/Hero.jsx
@@ -0,0 +1,70 @@
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+
+const Hero = () => {
+    return (
+        <section className={`relative w-full h-screen mx-auto`}>
+            <div
+                className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+            >
+                <div className="flex flex-col justify-center items-center mt-5">
+                    <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+                    <div className="w-1 sm:h-80 h-40 violet-gradient" />
+                </div>
+
+                <div style={{ zIndex: 1 }}>
+                    <h1 className={`${styles.heroHeadText} text-white`}>
+                        Soy{" "}
+                        <motion.span
+                            initial={{ opacity: 0, y: -20 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.5 }}
+                            className="text-[#915EFF]"
+                        >
+                            Andres Buendia
+                        </motion.span>
+                    </h1>
+                    <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+                        Ingeniero de Software | Desarrollador Front End
+                    </p>
+                    <div className="flex sm:flex-row mt-7">
+                        <a
+                            href="https://github.com/andresbuendia2020"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card"
+                        >
+                            <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">
+                                GitHub
+                            </button>
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/andres-buendia-41b127219/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card"
+                        >
+                            <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">
+                                LinkedIn
+                            </button>
+                        </a>
+                        <a
+                            href="https://drive.google.com/file/d/1pOK8GfH6cAEysrBOMBMXXWSjzmzNZmtJ/view?usp=sharing"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mb-4 sm:mb-0 sm:mr-4 green-pink-gradient p-[2px] rounded-[10px] shadow-card"
+                        >
+                            <button className="bg-[#151131] text-white px-4 py-2 rounded-lg transition duration-300 ease-in-out transform hover:scale-95">
+                                <strong>CV</strong>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            {/* <ComputersCanvas /> */}
+        </section>
+    );
+};
+
+export default Hero;
